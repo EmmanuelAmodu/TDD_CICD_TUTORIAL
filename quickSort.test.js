3d8adcdcf8eec71847ff6062121719c4317b8a36
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2019-08-18 19:39:24 +0100

    quick sort done, fix test

--- quickSort.test.js
+++ quickSort.test.js
@@ -1,5 +1,5 @@
 const quickSort = require('./quickSort');
 
 test('Passing arg [1, 4, 5, 3, 2] to quickSort should return [1, 2, 3, 4, 5]', () => {
-    expect(quickSort([1, 4, 5, 3, 2], 4, 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(quickSort([1, 4, 5, 3, 2], 0, 4)).toEqual([1, 2, 3, 4, 5]);
 });
